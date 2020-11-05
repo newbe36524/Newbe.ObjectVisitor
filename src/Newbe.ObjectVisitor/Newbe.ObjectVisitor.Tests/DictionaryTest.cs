a6bb6589bb39c7653ce5f4f330f6ebe43c3fd170
Traceless <traceless0929@outlook.com>
2020-11-05 09:36:51 +0800

    1

---
+++ src/Newbe.ObjectVisitor/Newbe.ObjectVisitor.Tests/DictionaryTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Newbe.ObjectVisitor.Tests
+{
+    public class DictionaryTest
+    {
+        [Test]
+        public void ToDic()
+        {
+            var obj = Yueluo.Create();
+            var dic = obj.V().CollectAsDictionary();
+            Console.WriteLine(dic);
+        }
+    }
+}
---
+++ src/Newbe.ObjectVisitor/Newbe.ObjectVisitor/Util/DictionaryExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Newbe.ObjectVisitor
+{
+    public static class DictionaryExtensions
+    {
+        public static Dictionary<string,object> CollectAsDictionary<T>(this T obj)
+        {
+            var dic = new Dictionary<string,object>();
+            DictionaryVisitor<T>.Instance.Run(obj, dic);
+            return dic;
+        }
+
+        private static class DictionaryVisitor<T>
+        {
+            internal static readonly ICachedObjectVisitor<T, Dictionary<string,object>> Instance = CreateDictionaryVisitor();
+            private static ICachedObjectVisitor<T, Dictionary<string,object>> CreateDictionaryVisitor()
+            {
+                var re = default(T)!
+                    .V()
+                    .WithExtendObject<T, Dictionary<string,object>>()
+                    .ForEach((name, value, s) => s.Add(name,value))
+                    .Cache();
+                return re;
+            }
+        }
+    }
+}
